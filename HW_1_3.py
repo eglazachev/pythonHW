2f90b239e85687bdbe5dc2a3a24ca1917e6328e6
eglazachev <63977872+eglazachev@users.noreply.github.com>
2021-02-04 17:14:45 +0300

    Delete HW_1_3.py

--- HW_1_3.py
+++
@@ -1,3 +0,0 @@
-user_number = input('Enter a number:')
-result = int(user_number) + int(user_number + user_number) + int(user_number + user_number + user_number)
-print('So we get ', result, 'here as n+nn+nnn')
