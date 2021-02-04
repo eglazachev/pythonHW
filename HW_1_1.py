072e013cdb51d50b6d4c729468c92bf032ceb217
eglazachev <63977872+eglazachev@users.noreply.github.com>
2021-02-04 17:20:14 +0300

    Update HW_1_1.py

--- HW_1_1.py
+++ HW_1_1.py
@@ -2,6 +2,5 @@ user_name = input('Как Вас зовут?\nВведите Имя:')
 user_age = int(input('Сколько Вам лет?\nВведите возраст:'))
 user_city = input('Где Вы родились?\nВведите название города:')
 user_power_lift = float(input('Скок жмёшь лёжа? А?\nВведите массу в килограммах:'))
-
 print('\nПользователю с именем {0} {1} лет, родившись в городе {2} {0} жмёт {3} кг от груди!'
       .format(user_name, user_age, user_city, user_power_lift))
