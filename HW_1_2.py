5cf1ac548b529297ff8bb8925e390d8cb3746aaf
eglazachev <63977872+eglazachev@users.noreply.github.com>
2021-02-04 17:14:34 +0300

    Delete HW_1_2.py

--- HW_1_2.py
+++
@@ -1,5 +0,0 @@
-total_sec = int(input('Enter a quantity of seconds:'))
-seconds = total_sec % 60
-minutes = (total_sec // 60) % 60
-hours = total_sec // 3600
-print(hours, ':', minutes, ':', seconds)
